10b710ab5abf5be3685dcb7163a33ca2af917b1c
Xianghan Wang <coolwust@gmail.com>
2018-07-26 17:01:55 -0400

    Fix LuaDLL

--- scripts/build-xlua-project.ps1
+++ scripts/build-xlua-project.ps1
@@ -37,6 +37,30 @@ foreach ($file in [Directory]::GetFiles($srcDir, "*.cs", [SearchOption]::AllDire
     [Regex]::Replace((Get-Content -Path $file -Raw), "([^!])UNITY_EDITOR", '$1NULL', [RegexOptions]::Multiline) | Set-Content -Path $file -Encoding UTF8
 }
 
+$content = @'
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace XLua.LuaDLL
+{
+    public partial class Lua
+    {
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_pb(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadLuaProfobuf(System.IntPtr L)
+        {
+            return luaopen_pb(L);
+        }
+    }
+}
+'@
+[File]::WriteAllText("$srcDir\CustomLuaDLL.cs", $content, [Encoding]::UTF8)
+
+
 if ([Directory]::Exists("$outputDir\Editor")) {
     [Directory]::Delete("$outputDir\Editor", $true)
 }
