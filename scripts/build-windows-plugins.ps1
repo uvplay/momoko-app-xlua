ec090fc2d8e77322430cee912af97861334a871e
Xianghan Wang <coolwust@gmail.com>
2018-10-02 00:23:12 -0400

    Update Windows script

--- scripts/build-windows-plugins.ps1
+++ scripts/build-windows-plugins.ps1
@@ -2,81 +2,43 @@ using namespace System.IO
 using namespace System.Text
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
 $ErrorActionPreference = "Stop"
 
-$projectRoot = Join-Path -Path $PSScriptRoot -ChildPath ".." -Resolve
-$cacheDir = "$projectRoot\cache"
-$buildDir = "$projectRoot\builds"
-$outputDir = "$projectRoot\output"
-
-$cmakeVersion = "3.12.0"
-$xluaVersion = "48b6867060971714ff2c1148a43bd8a3121909af"
-$luapbVersion = "106b9089a547acf38f2fc6c043f9c74e20cba0be"
-
 try {
-    foreach ($dir in $cacheDir, $buildDir) {
-        if ([Directory]::Exists($dir)) {
-            [Directory]::Delete($dir, $true)
-        }
-        [Directory]::CreateDirectory($dir) | Out-Null
-    }
+    $projectRoot = Join-Path -Path $PSScriptRoot -ChildPath ".." -Resolve
+    $tempDir = "$projectRoot\cache"
+    $buildDir = "$projectRoot\builds"
+    [Directory]::CreateDirectory($tempDir) | Out-Null
+    [Directory]::CreateDirectory($buildDir) | Out-Null
 
-    # [CMake](https://cmake.org/download/)
-    $cmakeVersion = "3.12.0"
-    $uri = "https://cmake.org/files/v$($cmakeVersion.Substring(0, $cmakeVersion.LastIndexOf('.')))/cmake-$cmakeVersion-win64-x64.zip"
-    Invoke-WebRequest -Uri $uri -OutFile "$cacheDir\cmake-$cmakeVersion-win64-x64.zip"
-    Expand-Archive -Path "$cacheDir\cmake-$cmakeVersion-win64-x64.zip" -DestinationPath $cacheDir
-    [Directory]::Move("$cacheDir\cmake-$cmakeVersion-win64-x64", "$cacheDir\cmake")
+    $uri = "https://cmake.org/files/v3.12/cmake-3.12.2-win64-x64.zip"
+    Invoke-WebRequest -Uri $uri -OutFile "$tempDir\cmake-3.12.2-win64-x64.zip"
+    Expand-Archive -Path "$tempDir\cmake-3.12.2-win64-x64.zip" -DestinationPath $tempDir
+    $cmakeCmd = "$tempDir\cmake-3.12.2-win64-x64\bin\cmake.exe"
 
-    # https://github.com/Tencent/xLua
-    $uri = "https://github.com/Tencent/xLua/archive/$xluaVersion.zip"
-    Invoke-WebRequest -Uri $uri -OutFile "$cacheDir\xLua-$xluaVersion.zip"
-    Expand-Archive -Path "$cacheDir\xLua-$xluaVersion.zip" -DestinationPath $cacheDir
-    [Directory]::Move("$cacheDir\xLua-$xluaVersion", "$cacheDir\xlua")
+    $xLuaCommit = "68f9751c04341df317cd68db521b76e184ae4c94"
+    $uri = "https://github.com/Tencent/xLua/archive/$xLuaCommit.zip"
+    Invoke-WebRequest -Uri $uri -OutFile "$tempDir\xLua-$xLuaCommit.zip"
+    Expand-Archive -Path "$tempDir\xLua-$xLuaCommit.zip" -DestinationPath $tempDir
+    $xLuaDir = "$tempDir\xLua-$xLuaCommit"
 
-    # https://github.com/starwing/lua-protobuf
-    $uri = "https://github.com/starwing/lua-protobuf/archive/$luapbVersion.zip"
-    Invoke-WebRequest -Uri $uri -OutFile "$cacheDir\lua-protobuf-$luapbVersion.zip"
-    Expand-Archive -Path "$cacheDir\lua-protobuf-$luapbVersion.zip" -DestinationPath $cacheDir
-    [Directory]::Move("$cacheDir\lua-protobuf-$luapbVersion", "$cacheDir\xlua\build\lua-protobuf")
+    $x86ConfigDir = "$tempDir\configs\x86"
+    $x86BuildDir = "$buildDir\Plugins\x86"
+    [Directory]::CreateDirectory($x86ConfigDir) | Out-Null
+    [Directory]::CreateDirectory($x86BuildDir) | Out-Null
+    Set-Location -Path $x86ConfigDir
+    & "$cmakeCmd" -G "Visual Studio 15 2017" "$xLuaDir\build"
+    & "$cmakeCmd" --build $x86ConfigDir --config Release
+    [File]::Move("$x86ConfigDir\Release\xlua.dll", "$x86BuildDir\xlua.dll")
 
-    $listFile = "$cacheDir\xLua\build\CMakeLists.txt"
-    $content = @'
-set (LPB_SRC
-    lua-protobuf/pb.c
-)
-set_property(
-    SOURCE ${LPB_SRC}
-    APPEND
-    PROPERTY COMPILE_DEFINITIONS
-    LUA_LIB
-)
-list(APPEND THIRDPART_INC lua-protobuf)
-set (THIRDPART_SRC ${THIRDPART_SRC} ${LPB_SRC})
-'@ + "`r`n`r`n"
-    $content += [File]::ReadAllText($listFile)
-    [File]::WriteAllText($listFile, $content, [Encoding]::UTF8)
-
-    [Directory]::CreateDirectory("$buildDir\windows\x86_64") | Out-Null
-    Push-Location -Path "$buildDir\windows\x86_64"
-    & "$cacheDir\cmake\bin\cmake.exe" -G "Visual Studio 15 2017 Win64" "$cacheDir\xlua\build"
-    Pop-Location
-    & "$cacheDir\cmake\bin\cmake.exe" --build "$buildDir\windows\x86_64" --config Release
-    if (-not [Directory]::Exists("$outputDir\Plugins\x86_64")) {
-        [Directory]::CreateDirectory("$outputDir\Plugins\x86_64") | Out-Null
-    }
-    [File]::Move("$buildDir\windows\x86_64\Release\xlua.dll", "$outputDir\Plugins\x86_64\xlua.dll")
-
-    [Directory]::CreateDirectory("$buildDir\windows\x86") | Out-Null
-    Push-Location -Path "$buildDir\windows\x86"
-    & "$cacheDir\cmake\bin\cmake.exe" -G "Visual Studio 15 2017" "$cacheDir\xlua\build"
-    Pop-Location
-    & "$cacheDir\cmake\bin\cmake.exe" --build "$buildDir\windows\x86" --config Release
-    if (-not [Directory]::Exists("$outputDir\Plugins\x86")) {
-        [Directory]::CreateDirectory("$outputDir\Plugins\x86") | Out-Null
-    }
-    [File]::Move("$buildDir\windows\x86\Release\xlua.dll", "$outputDir\Plugins\x86\xlua.dll")
+    $x64ConfigDir = "$tempDir\configs\x86_64"
+    $x64BuildDir = "$buildDir\Plugins\x86_64"
+    [Directory]::CreateDirectory($x64ConfigDir) | Out-Null
+    [Directory]::CreateDirectory($x64BuildDir) | Out-Null
+    Set-Location -Path $x64ConfigDir
+    & "$cmakeCmd" -G "Visual Studio 15 2017 Win64" "$xLuaDir\build"
+    & "$cmakeCmd" --build $x64ConfigDir --config Release
+    [File]::Move("$x64ConfigDir\Release\xlua.dll", "$x64BuildDir\xlua.dll")
 }
 catch {
     Write-Output $PSItem | Format-List -Force
--- scripts/install-visual-cpp-build-tools.ps1
+++ scripts/install-visual-cpp-build-tools.ps1
@@ -1,17 +1,13 @@
 using namespace System.IO
 
 $projectRoot = Join-Path -Path $PSScriptRoot -ChildPath ".." -Resolve
-$cacheDir = "$projectRoot\cache"
-
-if ([Directory]::Exists($cacheDir)) {
-    [Directory]::Delete($cacheDir, $true)
-}
-[Directory]::CreateDirectory($cacheDir) | Out-Null
+$tempDir = "$projectRoot\temp"
+[Directory]::CreateDirectory($tempDir) | Out-Null
 
 # https://visualstudio.microsoft.com/downloads/
 $uri = "https://download.visualstudio.microsoft.com/download/pr/c6e2b90d-9051-44bd-aef1-4ef3bdf8f084/30b5724c490239eee9608d63225b994f/vs_buildtools.exe"
-Invoke-WebRequest -Uri $uri -OutFile "$cacheDir\vs_buildtools.exe"
+Invoke-WebRequest -Uri $uri -OutFile "$tempDir\vs_buildtools.exe"
 
 # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
 # https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids
-& "$cacheDir\vs_buildtools.exe" --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --passive
+& "$tempDir\vs_buildtools.exe" --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --passive
